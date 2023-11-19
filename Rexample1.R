4a6c74ae43356b09d45e88ba78be9ad5f1bfc26a
Greski <nj06gres@siena.edu>
2023-11-19 18:06:14 -0500

    added test example file with small features

---
+++ Rexample1.R
@@ -0,0 +1,14 @@
+#Noah Greski and Andrew Jop
+
+#testing plots
+plot(1:50)
+
+#testing operators and printing
+a <- 10
+b <- 5
+c <- 40
+
+#2nd assignment takes precedence
+b<-30
+
+print (a + b - c)
