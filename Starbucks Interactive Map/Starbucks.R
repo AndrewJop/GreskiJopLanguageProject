cb610709c7a6943734197b9da215a073840920d6
Greski <nj06gres@siena.edu>
2023-12-04 17:01:52 -0500

    having issues

--- Starbucks Interactive Map/Starbucks.R
+++ Starbucks Interactive Map/Starbucks.R
@@ -14,8 +14,6 @@ starbucks_data <- read.csv("Starbucks Interactive Map/starbucks_locations.csv")
 starbucks_data <- starbucks_data %>%
   filter(!is.na(Longitude) & !is.na(Latitude))
 
-starbucks_data <- starbucks_data %>%
-  filter(!is.na(Longitude) & !is.na(Latitude))
 
 
 starbucks_sf <- st_as_sf(starbucks_data, coords = c("Longitude", "Latitude"))
@@ -25,10 +23,10 @@ starbucks_map_world <- leaflet(data = starbucks_sf ) %>%
   addTiles() %>%
   addMarkers(
     clusterOptions = markerClusterOptions(),
-    popup = ~paste("<strong>Name:</strong> ", Brand, "<br>",
-                   "<strong>City:</strong> ", City, "<br>",
-                   "<strong>Country:</strong> ", Country),
-    label = ~paste(Brand)
+    popup = ~paste("<strong>Store Name:</strong> ", StoreName, "<br>",
+                   "<strong>Country:</strong> ", Country, "<br>",
+                   "<strong>State/Province:</strong> ", State/Province, "<br>"),
+    label = ~paste(StoreName)
   )
 
 
